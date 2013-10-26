c418323809fc8ddb7aebac249159f6cbdc8602d0
Ben Cullen-Kerney <ben@beancuke.com>
2013-10-26 10:31:36 -0700

    Add tests (better late than never)

--- test/test_jurisdiction.rb
+++ test/test_jurisdiction.rb
@@ -1,11 +1,36 @@
 require 'minitest_helper'
 
-class TestJurisdiction < MiniTest::Unit::TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::Foo::VERSION
+jurisdiction :foo do
+  def monkey_say
+    "EEEEK!"
+  end
+end
+
+jurisdiction :bar, using: :capybara do
+  def go_there
+    visit '/'
+  end
+end
+
+describe Jurisdiction do
+
+  it "should define a new module under Jurisdiction::" do
+    Jurisdiction.constants.must_include :Foo
   end
 
-  def test_it_does_something_useful
-    assert false
+  it "should define methods on that module" do
+    Jurisdiction::Foo.methods.must_include :monkey_say
+  end
+
+  it "should be accessible via the helper method" do
+    foo.monkey_say.must_equal "EEEEK!"
+  end
+
+  describe "using" do
+    
+    it "should extend functionality" do
+      Jurisdiction::Bar.methods.must_include :visit
+    end
+
   end
 end
